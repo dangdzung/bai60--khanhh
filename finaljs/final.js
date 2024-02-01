c0d0f5bd83de62ae7f31c3f1f0e0887311ff4c31
dangdzung <117427659+dangdzung@users.noreply.github.com>
2024-02-01 09:53:00 +0700

    done

--- finaljs/final.js
+++ finaljs/final.js
@@ -24,3 +24,20 @@ function handleClickOutMenu(event) {
         toggle.classList.add("fa-bars");
     }
 }
+function renderSweetAlert() {
+    const template = ` <div class="sweet-alert">
+      <i class="fa fa-check sweet-icon"></i>
+      <p class="sweet-text">Congratulations on learning JS</p>
+      </div>`;
+    document.body.insertAdjacentHTML("beforeend", template);
+}
+const button = document.querySelector(".button");
+button.addEventListener("click", function () {
+    renderSweetAlert();
+    const sweetItem = document.querySelector(".sweet-alert");
+    if (sweetItem) {
+        setTimeout(function () {
+            sweetItem.parentElement.removeChild(sweetItem);
+        }, 2000);
+    }
+});
